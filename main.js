11fbdc1720941bd79dda57f2f1991bd26ca00fa9
Amulya Reddy <amulyareddyk@yahoo.in>
2019-04-08 20:31:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function f2(){
 	document.getElementById('figure_section').innerHTML='<embed src="https://wakatime.com/share/@dd967101-cc38-4044-b52a-1990b7cb2849/6dcd7651-1e67-43a6-ad75-f36da93b89d3.svg"></embed>';
 }
 function f3(){
-	document.getElementById('figure_section').innerHTML='<embed src="https://wakatime.com/share/@dd967101-cc38-4044-b52a-1990b7cb2849/c23a824a-eda1-4d74-af3f-7280a3e4d74f.svg"></embed>';
+	document.getElementById('figure_section').innerHTML='<embed src="https://wakatime.com/share/@dd967101-cc38-4044-b52a-1990b7cb2849/4aa9d99c-7171-489d-9f78-e601eda2d0c6.svg"></embed>';
 }
 function f4(){
-	document.getElementById('figure_section').innerHTML='<embed src="https://wakatime.com/share/@dd967101-cc38-4044-b52a-1990b7cb2849/39b95a30-d4ac-46fd-bcf0-2b7ee78be26a.svg"></embed>';
+	document.getElementById('figure_section').innerHTML='<embed src="https://wakatime.com/share/@dd967101-cc38-4044-b52a-1990b7cb2849/e6930d24-0ba5-4ef9-95b6-4f09be5e9112.svg"></embed>';
 }
 // $(document).ready(function(){
 // 	$("#code30_id").on("click",function(){
